a3a24636872ee36adf28e53b958e9ae764d39191
lorenzo <Vous@exemple.com>
2021-04-22 16:09:21 +0200

    v0.1

--- App.java
+++ App.java
@@ -2,9 +2,7 @@ class App
 {
     public static void main(String[] args) 
     {
-        Liste liste = new Liste();
-        Commande c1 = new Commande(client, date, listeProduits)
-        c1.getPrix();
+      
         
     }
 }
